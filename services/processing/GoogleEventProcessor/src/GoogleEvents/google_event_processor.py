df0faac0b0bab429ef6d8b5921b5930f64024b9c
Erlend Gjelsvik <erlend@pop-os.localdomain>
2020-02-13 09:20:33 +0100

    Added comments and TODOs

--- services/processing/GoogleEventProcessor/src/GoogleEvents/google_event_processor.py
+++ services/processing/GoogleEventProcessor/src/GoogleEvents/google_event_processor.py
@@ -8,34 +8,46 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 def handler(event, context):
+	# Fetch all data from s3 first to insert all data in one DB-Session
 #	data = {}
 #	for record in event['Records']:
-#		data.update(get_data_from_s3(record['s3']['object']['key']))
+#		data.update(record['s3']['bucket']['name'], get_data_from_s3(record['s3']['object']['key']))
 
 	engine = get_engine()
-
 #	drop_table(engine)
-#	create_table(engine)
-#	
+#
 #	Session = sessionmaker(bind = engine)
 #	session = Session()
-#
-#	insert_data(session, "aaaaa", "My event", "bartek.kek@knowit.no", 111111111, 10000000, "aaaaa")
-#	print(get_all_data(session)[0].version)
-#	
+#	process_data(data)
 #	session.close()
 
-def get_data_from_s3(name):
+
+def process_data(data):
+###########################################TODO#####################################################
+#	Fetch google events from db for the next 24 hours
+#	Compare s3 data with db data
+#	If event from s3 does not exist in db then insert into db
+#	Else if event has same Id but different attributes insert again DO NOT DELETE PREVIOUS EVENT
+#	Else do nothing
+#	
+#	Reference: https://github.com/knowit/Dataplattform/tree/master/services/structured_mysql/update
+####################################################################################################
+	print("Function can't be empty")
+
+
+def get_data_from_s3(bucket, object):
 	client = boto3.client('s3')
 	response = client.get_object(
-		Bucket='dataplattform-eventbox-bucket',
-		Key=name)
+		Bucket=bucket,
+		Key=object)
 
 	return response['Body'].read().decode('utf-8')
 
 
 def get_engine():
-	# VIL EGENTLIG HA DETTE, MEN MÅ ORDNE NOE MED VPC OG NOE GREIER ¯\_(ツ)_/¯
+#	VIL EGENTLIG HA DETTE, MEN MÅ ORDNE NOE MED VPC OG NOE GREIER ¯\_(ツ)_/¯
+#	https://stackoverflow.com/questions/52134100/parameter-store-request-timing-out-inside-of-aws-lambda -- Tror dette er problemet
+#
 #	stage = os.getenv('STAGE')
 #	parameter_path = "/{}/rds/postgres/".format(stage)
 #	client = boto3.client('ssm')
@@ -48,7 +60,6 @@ def get_engine():
 #
 #	username = response_username['Parameter']['Value']
 #	password = response_password['Parameter']['Value']
-
 
 	username = os.getenv('DATABASE_USERNAME')
 	password = os.getenv('DATABASE_PASSWORD')
@@ -82,6 +93,7 @@ def insert_data(session, id, event_summary, creator, start, end, event_code, act
 
 
 def get_all_data(session):
+	# Returns list of Events for all objects in table
 	return session.query(Events).all()
 
 
@@ -91,17 +103,13 @@ class Events(Base):
 	__tablename__ = 'events'
 
 	id = Column(String(100), primary_key = True)
-	version = Column(Integer, primary_key = True, autoincrement=True)
+	created_timestamp = Column(BigInteger, primary_key = True)
 	event_summary = Column(String(255))
 	creator = Column(String(255))
-	start = Column(BigInteger)
-	end = Column(BigInteger)
+	start_timestamp = Column(BigInteger)
+	end_timestamp = Column(BigInteger)
+	
 
-
-class Events_Status(Base):
-	__tablename__ = 'events_status'
-
-	id = Column(String(100), primary_key = True)
 
 
 if __name__== "__main__":
