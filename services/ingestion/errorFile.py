33cef21fd9e02a03276aa7495174e07e373ca93d
hovdis <rune.hovde.95@gmail.com>
2020-03-17 10:43:14 +0100

    All the wrongs

---
+++ services/ingestion/errorFile.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+print ( "hello")
+if 1 <> 3:
+    print('heil' )
+
+
+if 1==2:
+        print("Over-indented")
+
+if 1==1:
+print("under-indented")
+
+if 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2:
+    print("long-ass line")
+
+if 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2 and 1=1 and 2=2:
+    print("Long, but not too long")
+
+
+a  =  [1  ,2,  3,3]
+
+#Comment without space
+#######Comment
+#####  comment
+
+
+
+
+
+
+print("Long space")
+if 1=1 and
+    2=2:
+    print("jhfals")
+# This doc-comment is very looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong
+# This doc-comment is very loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong
+
