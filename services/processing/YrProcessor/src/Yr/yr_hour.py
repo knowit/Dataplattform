3299ba6f69c8428b70beeb2aa7859d79531bfb80
hovdis <rune.hovde.95@gmail.com>
2020-03-20 08:19:42 +0100

    Update yr_poller to send dicts, and make yr_processor to send the data from s3 to database.

---
+++ services/processing/YrProcessor/src/Yr/yr_hour.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, Float, Integer, TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class WeatherHour(Base):
+    __tablename__ = 'weather'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time = Column(TIMESTAMP)
+    location = Column(String(100))
+    location_name = Column(String(100))
+    time_from = Column(TIMESTAMP)
+    time_to = Column(TIMESTAMP)
+    precipitation = Column(Float)
+    wind_speed = Column(Float)
+    temperature = Column(Float)
+    air_pressure = Column(Float)
