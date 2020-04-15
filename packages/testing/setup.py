1234ed9975f6c92d86f5eb7bffcd441dff6cff2c
Jens Kristoffer Retian Markussen <jens.markussen@knowit.no>
2020-04-15 09:13:14 +0200

    use namespace package to avoid name conflicts and add a utility func

--- packages/testing/setup.py
+++ packages/testing/setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='dataplattform_testing',
-    packages=find_packages(),
+    packages=find_namespace_packages(include=['dataplattform.*']),
     entry_points={"pytest11": ["dataplattform = dataplattform.testing.plugin"]},
-    install_requires=['pytest', 'pytest-env', 'pytest-mock', 'moto', 'dataclasses-json'],
+    install_requires=['pytest', 'pytest-env', 'pytest-mock', 'moto', 'dataclasses-json', 'boto3'],
     zip_safe=False)
