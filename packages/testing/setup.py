109c89925565b06bdada52a407a7a8fa5af379b8
Jens Kristoffer Retian Markussen <jens.markussen@knowit.no>
2020-04-27 17:17:04 +0200

    fix tests and partition format for ubw fag poller

--- packages/testing/setup.py
+++ packages/testing/setup.py
@@ -4,5 +4,5 @@ setup(
     name='dataplattform_testing',
     packages=find_namespace_packages(include=['dataplattform.*']),
     entry_points={"pytest11": ["dataplattform = dataplattform.testing.plugin"]},
-    install_requires=['pytest', 'pytest-env', 'pytest-mock', 'moto', 'dataclasses-json', 'boto3'],
+    install_requires=['pytest', 'pytest-env', 'pytest-mock', 'moto', 'dataclasses-json', 'boto3', 'pyathena'],
     zip_safe=False)
