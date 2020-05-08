3357d29a8478dccc965e1b608ab09edf3a0ebf1a
Jens Kristoffer Retian Markussen <jens.markussen@knowit.no>
2020-05-08 07:35:17 +0200

    use handler scaffolding in jira setup poller to ensure common behaviour

--- packages/cli/dataplattform/cli/datasource/jira.py
+++ packages/cli/dataplattform/cli/datasource/jira.py
@@ -1,10 +1,13 @@
-from dataplattform.common.aws import SSM, S3
+from dataplattform.common.aws import SSM
 from dataplattform.common.schema import Data, Metadata
+from dataplattform.common.handler import Handler
 from requests import get, post, put
 from requests.auth import HTTPBasicAuth
 from datetime import datetime
 from dateutil.parser import isoparse
+import numpy as np
 import pandas as pd
+from os import environ
 
 
 def register_or_update_webhooks(stage: str):
@@ -47,65 +50,64 @@ def register_or_update_webhooks(stage: str):
 
 
 def poll_old_data(stage: str):
-    password, username = SSM(
-        with_decryption=True,
-        path=f'{stage}/jiraSalesWebhook'
-    ).get('jira_sales_password', 'jira_sales_username')
+    environ['STAGE'] = stage
+    environ['SERVICE'] = 'jiraSalesWebhook'
 
-    search_url = SSM(
-        with_decryption=False,
-        path=f'{stage}/jiraSalesWebhook'
-    ).get('jira_sales_search_url')
-
-    res = get(search_url, auth=HTTPBasicAuth(username, password), json={
-        'jql': "project = SALG and status != 'Rejected'",
-        'fields': 'labels, status, created, updated'
-    })
-
-    data = [
-        {
-            'issue': item['key'],
-            'customer': item['fields']['labels'][0] if len(item['fields']['labels']) > 0 else '',
-            'issue_status': item['fields']['status']['name'],
-            'created': int(isoparse(item['fields']['created']).timestamp()),
-            'updated': int(isoparse(item['fields']['updated']).timestamp())
-        }
-        for item in
-        res.json().get('issues', [])
-    ]
-
-    timestamp = datetime.now().timestamp()
-    s3 = S3(
+    handler = Handler(
         access_path=f'data/level-2/jira/sales',
-        bucket=f'{stage}-datalake-datalake'  # queryable?
-    )
+        bucket=f'{stage}-datalake-datalake')
 
-    s3.put(
-        Data(
+    @handler.ingest()
+    def ingest(event):
+        password, username = SSM(
+            with_decryption=True,
+        ).get('jira_sales_password', 'jira_sales_username')
+
+        search_url = SSM(
+            with_decryption=False,
+        ).get('jira_sales_search_url')
+
+        res = get(search_url, auth=HTTPBasicAuth(username, password), json={
+            'jql': "project = SALG and status != 'Rejected'",
+            'fields': 'labels, status, created, updated'
+        })
+
+        data = [
+            {
+                'issue': item['key'],
+                'customer': item['fields']['labels'][0] if len(item['fields']['labels']) > 0 else '',
+                'issue_status': item['fields']['status']['name'],
+                'created': int(isoparse(item['fields']['created']).timestamp()),
+                'updated': int(isoparse(item['fields']['updated']).timestamp())
+            }
+            for item in
+            res.json().get('issues', [])
+        ]
+
+        return Data(
             metadata=Metadata(
-                timestamp=timestamp
+                timestamp=datetime.now().timestamp()
             ),
             data=data
-        ),
-        'raw'
-    )
+        )
 
-    print(f'Uploaded rawdata {len(data)} old issues to {stage} S3 bucket')
+    @handler.process(partitions={
+        'jira_issue_created': ['issue_status'],
+        'jira_issue_updated': ['issue_status'],
+    })
+    def process(data):
+        data = [
+            [dict(x, time=d['metadata']['timestamp']) for x in d['data']]
+            for d in [d.json() for d in data]
+        ]
+        data = np.hstack(data)
 
-    df = pd.DataFrame.from_records(data)
-    table_names = ['jira_issue_created', 'jira_issue_updated']
-    for table_name in table_names:
-        df.to_parquet(f'structured/{table_name}',
-                      engine='fastparquet',
-                      compression='gzip',
-                      index=False,
-                      partition_cols=['issue_status'],
-                      file_scheme='hive',
-                      mkdirs=lambda x: None,  # noop
-                      open_with=s3.fs.open,
-                      append=s3.fs.exists(f'structured/{table_name}/_metadata'))
+        return {
+            'jira_issue_created': pd.DataFrame.from_records(data),
+            'jira_issue_updated': pd.DataFrame.from_records(data)
+        }
 
-    print(f'{", ".join(table_names)} tables updated with new parquet data')
+    handler(None)
 
 
 def setup(stage: str):
