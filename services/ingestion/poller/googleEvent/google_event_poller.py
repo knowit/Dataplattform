761c0640bd7ed57383a697c5ae31ad118d7b139e
HeleneSemmerud <helene.semmerud@knowit.no>
2020-05-08 15:45:35 +0200

    Tests sync time validity and different time formats

--- services/ingestion/poller/googleEvent/google_event_poller.py
+++ services/ingestion/poller/googleEvent/google_event_poller.py
@@ -9,7 +9,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 from botocore.exceptions import ClientError
 
 import json
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
+from pytz import timezone as pytz_timezone
 import pandas as pd
 from typing import Dict
 
@@ -34,26 +35,25 @@ def ingest(event) -> Data:
         """
         :param creds: credentials
         :param calendar_id:
-        :return: A dictionary containing the latest events using the sync token
+        :return: A dictionary containing the latest events using the latest poll date
         specific calendar_id.
         """
-        sync_token_ssm_name = ('sync_token_' + calendar_id).replace('@', '-', 1)
-        sync_token = None
+        last_poll_time_ssm_name = ('last_sync_time_' + calendar_id).replace('@', '-', 1)
+        last_poll_time = None
 
         try:
-            sync_token = SSM(with_decryption=False).get(sync_token_ssm_name)
+            last_poll_time = SSM(with_decryption=False).get(last_poll_time_ssm_name)
         except ClientError as e:
             if (e.response['Error']['Code'] == 'ParameterNotFound'):
-                sync_token = None
+                last_poll_time = None
             else:
                 raise e
 
-        events, new_sync_token = sync_events(service, calendar_id, sync_token)
-        SSM(with_decryption=False).put(name=sync_token_ssm_name,
-                                       value=new_sync_token,
+        events, new_poll_time = sync_events(service, calendar_id, last_poll_time)
+        SSM(with_decryption=False).put(name=last_poll_time_ssm_name,
+                                       value=new_poll_time,
                                        value_type='String',
                                        overwrite=True)
-
         return events
 
     def get_calender_service(credentials_from_env):
@@ -68,64 +68,59 @@ def ingest(event) -> Data:
 
         return service
 
-    def sync_events(service, calendar_id, sync_token):
+    def sync_events(service, calendar_id, last_poll_time):
 
-        min_date = '2020-05-01T00:00:00+00:00' # TODO: Fix to 2010-01-01T00:00:00+02:00
+        min_date = '2010-01-01T00:00:00+00:00'
+        today = datetime.now(timezone.utc)
+
         request_params = {'calendarId': calendar_id,
-                          'singleEvents': True}
-        if not sync_token:
+                          'singleEvents': True,
+                          'showDeleted': False,
+                          'orderBy': 'startTime',
+                          'timeMax': today.isoformat()}
+        if not last_poll_time:  # First time poller is called
             request_params['timeMin'] = min_date
         else:
-            request_params['syncToken'] = sync_token
+            last_sync_date = datetime.fromtimestamp(int(last_poll_time))
+            last_sync_date_rfc3339 = last_sync_date.astimezone(pytz_timezone("Europe/Oslo"))
+            request_params['timeMin'] = last_sync_date_rfc3339.isoformat()
 
         events_for_current_calender = []
         current_request = service.events().list(**request_params)
 
-        # TODO: Check if this is ok, should use pageToken?
-        pageToken = 'firstPage'
-        while pageToken != '':
-            try:
-                current_page = current_request.execute()
-            except errors.HttpError as e:
-                error_code = e.resp.status
-                # Sync token is outdated, do full sync
-                if error_code == 410:
-                    request_params.pop('syncToken', '')
-                    request_params['timeMin'] = min_date
-                    current_request = service.events().list(**request_params)
-                    current_page = current_request.execute()
-                else:
-                    raise e
-
+        while True:
+            current_page = current_request.execute()
             tmp_events = current_page.get('items', [])
             for event in tmp_events:
                 events_for_current_calender.append(get_event_info(event, calendar_id))
 
-            new_sync_token = current_page.get('nextSyncToken', '')
-            pageToken = current_page.get('nextPageToken', '')
+            page_token = current_page.get('nextPageToken')
+            if not page_token:
+                break
             current_request = service.events().list_next(current_request, current_page)
-        return events_for_current_calender, new_sync_token
+        return events_for_current_calender, str(int(today.timestamp()))
 
     def get_event_info(event, calendar_id):
 
         temp = event.get('location', '').split(',')
         boxes = [box.split('-')[-1] for box in temp if 'Enheter' in box]
-        creator_name = dict(event.get('creator', '')).get('email', '')
+        creator_email = dict(event.get('creator', '')).get('email', '')
+        event_timezone = event.get('timeZone', '')
 
         event_info = {
                 'event_id': event['id'],
                 'calendar_id': calendar_id,
-                'timestamp_from': get_time_from_event_time(event['start']),
-                'timestamp_to': get_time_from_event_time(event['end']),
+                'timestamp_from': get_timestamp_from_event_time(event['start'], event_timezone),
+                'timestamp_to': get_timestamp_from_event_time(event['end'], event_timezone),
                 'event_summary': event.get('summary', ''),
                 'event_button_names': boxes,
-                'creator': creator_name,
+                'creator': creator_email,
             }
         return event_info
     return Data(metadata=Metadata(timestamp=datetime.now().timestamp()), data=get_event_data())
 
 
-@handler.process(partitions={})  
+@handler.process(partitions={})
 def process(data) -> Dict[str, pd.DataFrame]:
     def make_dataframe(d):
         d = d.json()
@@ -140,14 +135,21 @@ def process(data) -> Dict[str, pd.DataFrame]:
     }
 
 
-def get_time_from_event_time(start_or_end_time):
-    if "dateTime" in start_or_end_time:
-        new_time = start_or_end_time['dateTime']
-    else:
+def get_timestamp_from_event_time(start_or_end_time, event_timezone_str):
+    if 'dateTime' in start_or_end_time:
+        new_time_str = start_or_end_time.get('dateTime')
+        new_time = datetime.fromisoformat(new_time_str)
+        if (event_timezone_str != ''):
+            event_timezone = pytz_timezone(event_timezone_str)
+            new_time = datetime.fromisoformat(event_timezone.localize(new_time))
+    elif 'date' in start_or_end_time:
         new_time = get_datetime_from_date(start_or_end_time)
-    return new_time
+    else:
+        raise KeyError
+
+    return int(new_time.timestamp())
 
 
 def get_datetime_from_date(start_or_end_date):
-    timeObject = list(map(int, start_or_end_date['date'].split("-")))
-    return datetime(timeObject[0], timeObject[1], timeObject[2])
+    time_object = list(map(int, start_or_end_date['date'].split("-")))
+    return datetime(time_object[0], time_object[1], time_object[2])
--- services/ingestion/poller/googleEvent/test_google_event_poller.py
+++ services/ingestion/poller/googleEvent/test_google_event_poller.py
@@ -2,7 +2,9 @@ from google_event_poller import handler
 from pytest import fixture
 from json import loads
 from datetime import datetime
+from botocore.exceptions import ClientError
 import copy
+from time import sleep
 
 
 @fixture
@@ -41,77 +43,76 @@ def events_list_mock_mult_calendars(mocker):
     service_mock.events.return_value = events_mock
     mock_service_builder.return_value = service_mock
 
-    def payload(data):
+    def payload():
         def list_side_effect(**params):
+            def generate_cal_events(calendar_id):
+                start_time = datetime(2019, 6, 15+int(calendar_id) - 1, 18, 30)
+                end_time = datetime(2019, 6, 15+int(calendar_id) - 1, 19, 30)
+                test_events = {'items': [{
+                    'id': str(1234 + int(calendar_id)),
+                    'start': {
+                        'dateTime': start_time.isoformat()
+                                },
+                    'end': {
+                        'dateTime': end_time.isoformat()
+                            },
+                    'location': 'Enheter-box' + calendar_id
+                    }]}
+                return test_events
             events_list_mock = mocker.MagicMock()
-            events_list_mock.execute.return_value = {**data, **{'syncToken': '123', 'calenderId': params['calendarId']}}
+            events_list_mock.execute.return_value = {**generate_cal_events(params['calendarId']),
+                                                     **{'calendarId': params['calendarId']}}
             return events_list_mock
-
         events_mock.list = mocker.MagicMock(
             side_effect=list_side_effect)
+
     yield payload
 
 
 def test_events_multiple_calendars(s3_bucket, events_list_mock_mult_calendars):
-    events_list_mock_mult_calendars({
-        'items':
-        [{
-            'id': '1234',
-            'start': {
-                'dateTime': ''
-            },
-            'end': {
-                'dateTime': ''
-            },
-            'location': 'Enheter-box1'
-            },
-            {
-            'id': '1235',
-            'start': {
-                'dateTime': ''
-            },
-            'end': {
-                'dateTime': ''
-            }
-        }]
-    })
+    start_time_1 = int(datetime(2019, 6, 15, 18, 30).timestamp())
+    end_time_1 = int(datetime(2019, 6, 15, 19, 30).timestamp())
 
-    expected = [{'event_id': '1234',
+    start_time_2 = int(datetime(2019, 6, 16, 18, 30).timestamp())
+    end_time_2 = int(datetime(2019, 6, 16, 19, 30).timestamp())
+
+    events_list_mock_mult_calendars()
+    expected = [{'event_id': '1235',
                  'calendar_id': '1',
-                 'timestamp_from': '',
-                 'timestamp_to': '',
+                 'timestamp_from': start_time_1,
+                 'timestamp_to': end_time_1,
                  'event_summary': '',
                  'event_button_names': ['box1'],
                  'creator': ''
-                 },
-                {'event_id': '1235',
-                 'calendar_id': '1',
-                 'timestamp_from': '',
-                 'timestamp_to': '',
-                 'event_summary': '',
-                 'event_button_names': [],
-                 'creator': ''
+                 }]
+    expected2 = [{'event_id': '1236',
+                  'calendar_id': '2',
+                  'timestamp_from': start_time_2,
+                  'timestamp_to': end_time_2,
+                  'event_summary': '',
+                  'event_button_names': ['box2'],
+                  'creator': ''
                 }]
-    expected2 = copy.deepcopy(expected)
-    expected2[0]['calendar_id'] = '2'
-    expected2[1]['calendar_id'] = '2'
     handler(None, None)
-    expected_new = [expected[0], expected[1], expected2[0], expected2[1]]
-    
+
+    expected_new = [expected[0], expected2[0]]
     response = s3_bucket.Object(next(iter(s3_bucket.objects.all())).key).get()
     data = loads(response['Body'].read())
     assert [i for i in data['data'] if i not in expected_new] == []
 
 
 def test_events_pagination(s3_bucket, events_list_mock):
+    start_time_1 = datetime(2019, 6, 15, 18, 30)
+    end_time_1 = datetime(2019, 6, 15, 19, 30)
+
     items1 = {
         'items': [{
             'id': '1234',
             'start': {
-                'dateTime': ''
+                'dateTime': start_time_1.isoformat()
             },
             'end': {
-                'dateTime': ''
+                'dateTime': end_time_1.isoformat()
             },
             'location': 'Enheter-box1'
             }]}
@@ -119,10 +120,10 @@ def test_events_pagination(s3_bucket, events_list_mock):
         'items': [{
             'id': '1235',
             'start': {
-                'dateTime': ''
+                'dateTime': start_time_1.isoformat()
             },
             'end': {
-                'dateTime': ''
+                'dateTime': end_time_1.isoformat()
             },
             'location': ''
             }]}
@@ -131,16 +132,16 @@ def test_events_pagination(s3_bucket, events_list_mock):
 
     expected = [{'event_id': '1234',
                  'calendar_id': '1',
-                 'timestamp_from': '',
-                 'timestamp_to': '',
+                 'timestamp_from': int(start_time_1.timestamp()),
+                 'timestamp_to': int(end_time_1.timestamp()),
                  'event_summary': '',
                  'event_button_names': ['box1'],
                  'creator': ''
                  },
                 {'event_id': '1235',
                  'calendar_id': '1',
-                 'timestamp_from': '',
-                 'timestamp_to': '',
+                 'timestamp_from': int(start_time_1.timestamp()),
+                 'timestamp_to': int(end_time_1.timestamp()),
                  'event_summary': '',
                  'event_button_names': [],
                  'creator': ''
@@ -156,40 +157,116 @@ def test_events_pagination(s3_bucket, events_list_mock):
     assert [i for i in data['data'] if i not in expected_new] == []
 
 
+def test_handler_time_format(s3_bucket, events_list_mock):
+    test_date = datetime(2014, 4, 3).date()
+    test_date2 = datetime(2014, 4, 4).date()
+    test_date_str = test_date.isoformat()
+    test_date2_str = test_date2.isoformat()
 
-def test_handler_write(s3_bucket, events_list_mock):
     events_list_mock({
         'items': [{
             'id': '1234',
             'start': {
-                'dateTime': datetime.utcnow().isoformat()
+                'date': test_date_str
             },
             'end': {
-                'dateTime': datetime.utcnow().isoformat()
+                'date': test_date2_str
             },
             'location': 'Enheter-box1'
-        },
-        {
-            'id': '1235',
-            'start': {
-                'dateTime': datetime.utcnow().isoformat()
-            },
-            'end': {
-                'dateTime': datetime.utcnow().isoformat()
-            }
-        }]
-    }, {})
+        }]},
+        {})
 
     handler(None, None)
+
+    expected = [{'event_id': '1234',
+                 'calendar_id': '1',
+                 'timestamp_from': int(datetime(test_date.year, test_date.month, test_date.day).timestamp()),
+                 'timestamp_to': int(datetime(test_date2.year, test_date2.month, test_date2.day).timestamp()),
+                 'event_summary': '',
+                 'event_button_names': ['box1'],
+                 'creator': ''
+                 },
+                {'event_id': '1234',
+                 'calendar_id': '2',
+                 'timestamp_from': int(datetime(test_date.year, test_date.month, test_date.day).timestamp()),
+                 'timestamp_to': int(datetime(test_date2.year, test_date2.month, test_date2.day).timestamp()),
+                 'event_summary': '',
+                 'event_button_names': ['box1'],
+                 'creator': ''
+                 }]
 
     response = s3_bucket.Object(next(iter(s3_bucket.objects.all())).key).get()
     data = loads(response['Body'].read())
 
-    assert '1234' in data['data'][0]['event_id'] and '1235' in data['data'][1]['event_id']
+    assert [i for i in data['data'] if i not in expected] == []
 
 
+def test_handler_time_format_timezone(s3_bucket, events_list_mock):
+    start_time_1 = datetime(2019, 6, 15, 18, 30)
+    end_time_1 = datetime(2019, 6, 15, 19, 30)
 
-# TODO:
-# Test time formats
-# Test error handling
-# Filter future events 
+    events_list_mock({
+        'items': [{
+            'id': '1234',
+            'start': {
+                'dateTime': start_time_1.isoformat(),
+                'timeZone': 'Europe/Oslo',
+            },
+            'end': {
+                'dateTime': end_time_1.isoformat(),
+                'timeZone': 'Europe/Oslo'
+            },
+            'location': 'Enheter-box1'
+        }]},
+        {})
+
+    handler(None, None)
+
+    expected = [{'event_id': '1234',
+                 'calendar_id': '1',
+                 'timestamp_from': int(start_time_1.timestamp()),
+                 'timestamp_to': int(end_time_1.timestamp()),
+                 'event_summary': '',
+                 'event_button_names': ['box1'],
+                 'creator': ''
+                 },
+                {'event_id': '1234',
+                 'calendar_id': '2',
+                 'timestamp_from': int(start_time_1.timestamp()),
+                 'timestamp_to': int(end_time_1.timestamp()),
+                 'event_summary': '',
+                 'event_button_names': ['box1'],
+                 'creator': ''
+                 }]
+
+    response = s3_bucket.Object(next(iter(s3_bucket.objects.all())).key).get()
+    data = loads(response['Body'].read())
+
+    assert [i for i in data['data'] if i not in expected] == []
+
+
+def test_handler_set_and_get_from_ssm(events_list_mock, ssm_client):
+    events_list_mock({}, {})
+
+    expectionRaised = False
+    try:
+        last_sync_time_for_cal1 = ssm_client.get_parameter(Name='/dev/testService/last_sync_time_1',
+                                                           WithDecryption=False)['Parameter']['Value']
+    except ClientError as e:
+        expectionRaised = True
+        assert (e.response['Error']['Code'] == 'ParameterNotFound')
+
+    handler(None, None)
+
+    last_sync_time_for_cal1 = ssm_client.get_parameter(Name='/dev/testService/last_sync_time_1',
+                                                       WithDecryption=False)['Parameter']['Value']
+
+    sleep(10)
+    handler(None, None)
+
+    new_sync_time_for_cal1 = ssm_client.get_parameter(Name='/dev/testService/last_sync_time_1',
+                                                      WithDecryption=False)['Parameter']['Value']
+
+    sync_time_is_updated = new_sync_time_for_cal1 != last_sync_time_for_cal1
+
+    assert last_sync_time_for_cal1 is not None and expectionRaised and sync_time_is_updated
