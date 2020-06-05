dedacbd40e5cc66792f472becdd2c37e2222501b
HeleneSemmerud <helene.semmerud@knowit.no>
2020-06-05 09:32:59 +0200

    --amend

--- services/ingestion/webHooks/googleSheets/google_sheets_webhook.py
+++ services/ingestion/webHooks/googleSheets/google_sheets_webhook.py
@@ -7,15 +7,13 @@ from typing import Dict
 
 handler = Handler()
 
+
 @handler.ingest()
 def ingest(event) -> Data:
 
-    print("type", type(event['body']))
-    print("value", event['body'])
-
     return Data(
         Metadata(timestamp=int(datetime.now().timestamp())),
-        data=json.loads(event['body'])
+        data=json.loads(event.get('body', "{}"))
     )
 
 
@@ -24,14 +22,18 @@ def process(data) -> Dict[str, pd.DataFrame]:
     def make_dataframes(d):
         d = d.json()
         metadata, payload = d['metadata'], d['data']
-        print("payload: ", payload)
-        table_name = payload['tableName']
-        table_content = payload['values']
-        data_df = pd.DataFrame(table_content[1:], columns=table_content[0])
-        table_name = table_name.replace(" ", "_")  # TODO: other not allowed chars?
-        data_df = pd.json_normalize(payload)
-        
-        metadata_df = pd.DataFrame({'author': [payload['email']], 'timestamp': [metadata['timestamp']]})
+        timestamp = metadata['timestamp']
+        table_name = payload.get('tableName', 'untitled' + str(timestamp))  # TODO: Add warning?
+        table_content = payload.get('values', None)
+        data_df = pd.DataFrame()
+
+        if table_content:
+            data_df = pd.DataFrame(table_content[1:], columns=table_content[0])
+            table_name = table_name.replace(" ", "_")  # TODO: other not allowed chars?
+
+        metadata_df = pd.DataFrame({'author': [payload.get('email', 'undefined')],
+                                    'time_added_to_dataplattform': [metadata['timestamp']],
+                                    'inserted_tables': [table_name]})
         return table_name, metadata_df, data_df
 
     table_name, metadata_df, data_df = make_dataframes(data[0])
--- services/ingestion/webHooks/googleSheets/test_google_sheets_webhook.py
+++ services/ingestion/webHooks/googleSheets/test_google_sheets_webhook.py
@@ -10,34 +10,38 @@ from pandas.testing import assert_frame_equal
 
 
 @fixture
-def test_data():
+def test_data1():
     with open(path.join(path.dirname(__file__), 'test_data1.json'), 'r') as json_file:
         yield json_file.read()
 
 
-def test_insert_data(s3_bucket, test_data):
+@fixture
+def test_data2():
+    with open(path.join(path.dirname(__file__), 'test_data2.json'), 'r') as json_file:
+        yield json_file.read()
+
+
+def test_insert_data(s3_bucket, test_data1):
     handler(APIGateway(
         headers={},
-        body=test_data).to_dict(), None)
+        body=test_data1).to_dict(), None)
 
     response = s3_bucket.Object(next(iter(s3_bucket.objects.all())).key).get()
     data = loads(response['Body'].read())
-    print("In test file", data)
+    assert data['metadata']['event'] == 'test' and\
+        data['data']['id'] == 186853002
 
+    
     assert True
 
 
-def test_process_parquet_get_data(mocker, create_table_mock, test_data):    
+def test_process_parquet_get_data(mocker, create_table_mock, test_data1):
     handler(APIGateway(
         headers={},
-        body=test_data).to_dict(), None)
+        body=test_data1).to_dict(), None)
 
     create_table_mock.assert_table_data_column(
         'google_sheets_metadata',
         'author',
         pd.Series(['test@test.b']))
 
-
-# TODO: Filtrere bort blanke rader/kolonner
-# TODO: Test metadata
-# TODO: 
