8801f0f8447f10a2055e56ca2f4a82cd9e379f32
HeleneSemmerud <helene.semmerud@knowit.no>
2020-06-29 19:30:51 +0200

    Deletes first try

--- templates/lambda1/test_my_ingest_service.py
+++
@@ -1,6 +0,0 @@
-from my_ingest_service import handler
-
-
-def test_pass():
-    handler(None)
-    assert True
--- templates/lambda2/test_my_service.py
+++
@@ -1,6 +0,0 @@
-from my_service import handler
-
-
-def test_pass():
-    handler(None)
-    assert True
