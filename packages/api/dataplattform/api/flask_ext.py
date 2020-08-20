c7cdec7189f6fff60083b66f1baeaf51229cd811
Jens Kristoffer Retian Markussen <jens.markussen@knowit.no>
2020-08-20 13:27:02 +0200

    cache cognito request and boto sessions for reuse

--- packages/api/dataplattform/api/flask_ext.py
+++ packages/api/dataplattform/api/flask_ext.py
@@ -4,48 +4,55 @@ import datetime
 from dateutil.tz import tzlocal
 from flask import Flask, request
 import re
-
-
-def create_session(role_arn: str, base_session=None):
-    base_session = base_session or boto3.session.Session()._session
-    fetcher = botocore.credentials.AssumeRoleCredentialFetcher(
-        client_creator=base_session.create_client,
-        source_credentials=base_session.get_credentials(),
-        role_arn=role_arn)
-    creds = botocore.credentials.DeferredRefreshableCredentials(
-        method='assume-role',
-        refresh_using=fetcher.fetch_credentials,
-        time_fetcher=lambda: datetime.datetime.now(tzlocal()))
-
-    botocore_session = botocore.session.Session()
-    botocore_session._credentials = creds
-    return boto3.Session(botocore_session=botocore_session)
+from cachetools import cached, TTLCache
 
 
 class UserSession(object):
-    def __init__(self, app: Flask = None):
+    def __init__(self, app: Flask = None, assume_user_role=True):
         if app is not None:
             self.init_app(app)
 
-    def init_app(self, app: Flask):
-        app.before_request(self.assume_role)
+    def init_app(self, app: Flask, assume_user_role=True):
+        self.base_session = boto3._get_default_session()
+        if assume_user_role:
+            app.before_request(self.assume_role)
 
+    def assume_role(self):
+        boto3.setup_default_session(botocore_session=self.boto_session._session)
+
+    @property
     def user_claims(self):
         event = request.environ.get('serverless.event', {})
         return event.get('requestContext', {}).get('authorizer', {}).get('claims', None)
 
-    def cognito_group(self):
-        claims = self.user_claims()
-        if claims:
-            cognito_client = boto3.client('cognito-idp')
-            response = cognito_client.get_group(GroupName=re.sub(r"[\[\]]", "", claims['cognito:groups']),
-                                                UserPoolId=claims['iss'].split('/')[-1])
-            return response['Group']
-        return None
-
+    @property
     def boto_session(self):
-        group = self.cognito_group()
-        return create_session(group['RoleArn']) if group else boto3._get_default_session()
+        claims = self.user_claims
+        if claims:
+            group = self.cognito_group(claims['cognito:groups'], claims['iss'])
+            if group:
+                return self.create_boto_session(group['RoleArn'])
+        return boto3._get_default_session()
 
-    def assume_role(self):
-        boto3.setup_default_session(botocore_session=self.boto_session()._session)
+    @cached(TTLCache(maxsize=10, ttl=60))
+    def cognito_group(self, group: str, iss: str):
+        cognito_client = self.base_session.client('cognito-idp')
+        response = cognito_client.get_group(
+            GroupName=re.sub(r"[\[\]]", "", group),
+            UserPoolId=iss.split('/')[-1])
+        return response.get('Group', None)
+
+    @cached(TTLCache(maxsize=10, ttl=60))
+    def create_boto_session(self, role_arn: str):
+        fetcher = botocore.credentials.AssumeRoleCredentialFetcher(
+            client_creator=self.base_session._session.create_client,
+            source_credentials=self.base_session._session.get_credentials(),
+            role_arn=role_arn)
+        creds = botocore.credentials.DeferredRefreshableCredentials(
+            method='assume-role',
+            refresh_using=fetcher.fetch_credentials,
+            time_fetcher=lambda: datetime.datetime.now(tzlocal()))
+
+        botocore_session = botocore.session.Session()
+        botocore_session._credentials = creds
+        return boto3.Session(botocore_session=botocore_session)
