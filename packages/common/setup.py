878f2e89b88e20b6cc91564e4ec53519dd661ee9
Jens Kristoffer Retian Markussen <jens.markussen@knowit.no>
2020-09-11 09:37:35 +0200

    need to limit moto version and s3fs (with aiobotocore) to avoid a regression error breaking tests

--- packages/common/setup.py
+++ packages/common/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='dataplattform_common',
     packages=find_namespace_packages(include=['dataplattform.*']),
-    install_requires=['dataclasses-json', 'boto3', 's3fs', 'fastparquet', 'pandas'],
+    install_requires=['dataclasses-json', 'boto3', 's3fs<0.5', 'fastparquet', 'pandas'],
     zip_safe=False)
