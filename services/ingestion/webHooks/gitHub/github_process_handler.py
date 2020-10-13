97ba1ba0e4bcd070c56e96e8873cfb94ba18417d
HeleneSemmerud <helene.semmerud@knowit.no>
2020-10-13 13:11:25 +0200

    Refactors github with sqs-queue, moves github poller code

---
+++ services/ingestion/webHooks/gitHub/github_process_handler.py
@@ -0,0 +1,50 @@
+from dataplattform.common.handlers.process import ProcessHandler
+from dataplattform.query.engine import Athena
+from typing import Dict
+import pandas as pd
+
+handler = ProcessHandler()
+ath = Athena()
+
+
+@handler.process(partitions={'github_knowit_repos': ['language', 'default_branch'],
+                             'github_knowit_repo_status': []})
+def process(data, events) -> Dict[str, pd.DataFrame]:
+    def make_dataframe(d):
+        d = d.json()
+        metadata, payload = d['metadata'], d['data']
+        repos_dataframe = pd.json_normalize(payload)
+        repos_dataframe['time'] = int(metadata['timestamp'])
+        return repos_dataframe
+
+    # TODO: Move to separate file, together with main table construction in ingest
+    repos_table_coloumns = ['id',
+                            'name',
+                            'description',
+                            'url',
+                            'html_url',
+                            'owner',
+                            'created_at',
+                            'language',
+                            'default_branch',
+                            'time']
+
+    repos_status_table_coloumns = ['id',
+                                   'updated_at',
+                                   'pushed_at',
+                                   'forks_count',
+                                   'stargazers_count',
+                                   'time']
+
+    github_dataframe = pd.concat([make_dataframe(d) for d in data])
+
+    reg_ids_df = ath.from_('github_knowit_repos').select('id').execute(ath).as_pandas()
+    github_dataframe = github_dataframe[~github_dataframe.id.isin(reg_ids_df.id)]
+
+    repos_table = github_dataframe.loc[:, repos_table_coloumns]
+    repos_status_table = github_dataframe.loc[:, repos_status_table_coloumns]
+
+    return {
+        'github_knowit_repos': repos_table,
+        'github_knowit_repo_status': repos_status_table
+    }
