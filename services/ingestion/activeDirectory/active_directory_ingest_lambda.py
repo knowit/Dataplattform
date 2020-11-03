cde82de0f07568de5f08d4a5efc95bfc9957c3fc
Long Hoang Ho <long.ho@knowit.no>
2020-11-03 09:50:50 +0100

    Add active directory

---
+++ services/ingestion/activeDirectory/active_directory_ingest_lambda.py
@@ -0,0 +1,25 @@
+from dataplattform.common.handlers.ingest import IngestHandler
+from dataplattform.common.schema import Data, Metadata
+from datetime import datetime
+import requests
+
+url = 'http://10.205.0.5:20201/api/Users'
+handler = IngestHandler()
+
+
+@handler.ingest()
+def ingest(event) -> Data:
+    res = requests.get(f'{url}')
+    data_json = res.json()
+
+    def get_list_of_users(data):
+        list_of_users = []
+        for user in data:
+            user_details = user['userDetails']
+            list_of_users.append(user_details)
+            print(user_details)
+            print("----------")
+            break
+        return list_of_users
+
+    return Data(metadata=Metadata(timestamp=datetime.now().timestamp()), data=get_list_of_users(data_json))
