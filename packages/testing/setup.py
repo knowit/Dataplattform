25483be99213c0a8752b399299a36d2819b06161
HeleneSemmerud <helene.semmerud@knowit.no>
2020-11-10 15:52:03 +0100

    Restrict to old versions of pyathena

--- packages/testing/setup.py
+++ packages/testing/setup.py
@@ -5,5 +5,5 @@ setup(
     packages=find_namespace_packages(include=['dataplattform.*']),
     entry_points={"pytest11": ["dataplattform = dataplattform.testing.plugin"]},
     install_requires=['pytest', 'pytest-env', 'pytest-mock', 'moto<1.3.15',
-                      'dataclasses-json', 'boto3', 'botocore', 'pyathena'],
+                      'dataclasses-json', 'boto3', 'botocore', 'pyathena<2.0.0'],
     zip_safe=False)
