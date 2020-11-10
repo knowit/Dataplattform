db1da03f333504ee87f7fd91729f83da19179c45
HeleneSemmerud <helene.semmerud@knowit.no>
2020-11-10 20:08:04 +0100

    Freeze versions in setup.py files

--- packages/testing/setup.py
+++ packages/testing/setup.py
@@ -4,6 +4,6 @@ setup(
     name='dataplattform_testing',
     packages=find_namespace_packages(include=['dataplattform.*']),
     entry_points={"pytest11": ["dataplattform = dataplattform.testing.plugin"]},
-    install_requires=['pytest', 'pytest-env', 'pytest-mock', 'moto<1.3.15',
-                      'dataclasses-json', 'boto3', 'botocore', 'pyathena<2.0.0'],
+    install_requires=['pytest==5.4', 'pytest-env==0.6.2', 'pytest-mock==3.1.0', 'moto==1.3.14',
+                      'dataclasses-json==0.4', 'boto3==1.14', 'botocore==1.17.44', 'pyathena==1.11'],
     zip_safe=False)
