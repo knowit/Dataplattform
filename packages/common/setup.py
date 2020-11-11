b6bab21095c45f16d30263c4b9719036981f2bdf
HeleneSemmerud <helene.semmerud@knowit.no>
2020-11-11 10:16:34 +0100

    Remove pinning of boto3 and botocore, which are included in lambda runtime, and therefore not under source control

--- packages/common/setup.py
+++ packages/common/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='dataplattform_common',
     packages=find_namespace_packages(include=['dataplattform.*']),
-    install_requires=['dataclasses-json==0.4', 'boto3==1.14', 's3fs==0.4', 'fastparquet==0.4', 'pandas==1.0'],
+    install_requires=['dataclasses-json==0.4', 'boto3', 's3fs==0.4', 'fastparquet==0.4', 'pandas==1.0'],
     zip_safe=False)
--- packages/testing/setup.py
+++ packages/testing/setup.py
@@ -5,5 +5,5 @@ setup(
     packages=find_namespace_packages(include=['dataplattform.*']),
     entry_points={"pytest11": ["dataplattform = dataplattform.testing.plugin"]},
     install_requires=['pytest==5.4', 'pytest-env==0.6.2', 'pytest-mock==3.1.0', 'moto==1.3.14',
-                      'dataclasses-json==0.4', 'boto3==1.14', 'botocore==1.17.44', 'pyathena==1.11'],
+                      'dataclasses-json==0.4', 'boto3', 'botocore', 'pyathena==1.11'],
     zip_safe=False)
