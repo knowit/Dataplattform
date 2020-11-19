58a1781dc1297a9ae07649025bd8b943349fd078
HeleneSemmerud <63657610+HeleneSemmerud@users.noreply.github.com>
2020-11-19 09:25:30 +0100

    Revert "Feature/pin pip package versions for consistency"

--- packages/query/setup.py
+++ packages/query/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='dataplattform_query',
     packages=find_namespace_packages(include=['dataplattform.*']),
-    install_requires=['pyathena==1.11', 'pandas==1.0', 'pypika==0.37'],
+    install_requires=['pyathena', 'pyathena[pandas]', 'pandas', 'pypika'],
     zip_safe=False)
