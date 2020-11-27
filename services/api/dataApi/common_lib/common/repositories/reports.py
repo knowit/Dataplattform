c44124f32726b43e8cd9cef1a56da7adf23c61c5
Christopher Gjøvåg <christopher.gjovag@knowit.no>
2020-11-27 16:04:51 +0100

    Add support for DataUpdate topic

--- services/api/dataApi/common_lib/common/repositories/reports.py
+++ services/api/dataApi/common_lib/common/repositories/reports.py
@@ -1,3 +1,4 @@
+from boto3.dynamodb.conditions import Attr
 from common.repositories.dynamo_db import DynamoDBRepository
 from os import environ
 from datetime import datetime
@@ -16,6 +17,14 @@ class ReportsRepository(DynamoDBRepository):
         )
         return response['Item']
 
+    def get_by_tables(self, tables: [str]):
+
+        def compare_table(table):
+            return table in tables
+
+        result = filter(lambda report: any(compare_table(t) for t in report['tables']), self.all())
+        return list(result)
+
     def all(self):
         response = self.table.scan()
         return response['Items']
@@ -30,13 +39,30 @@ class ReportsRepository(DynamoDBRepository):
                 'dataProtection': model['dataProtection'],
                 'created': now.isoformat(),
                 'lastUsed': None,
-                'lastCacheUpdate': now.isoformat()
+                'lastCacheUpdate': None
             },
             Expected={
                 'name': {'Exists': False}
             }
         )
         return True
+
+    def update_cache_time(self, name: str):
+        from botocore.exceptions import ClientError
+        now = datetime.now()
+        try:
+            self.table.update_item(
+                Key={'name': name},
+                AttributeUpdates={
+                    'lastCacheUpdate': {
+                        'Value': now.isoformat(),
+                        'Action': 'PUT'
+                    }
+                },
+                ConditionExpression=Attr("name").eq(name)
+            )
+        except ClientError:
+            raise KeyError("Could not find report with name: "+name)
 
     def delete(self, name: str):
         self.table.delete_item(
