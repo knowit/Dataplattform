c54b79aa8f844c38edff62c2a555ba0590b5dbde
Christopher Gjøvåg <christopher.gjovag@knowit.no>
2020-12-03 14:01:16 +0100

    Use dataplattform.common.aws.SNS

--- services/api/dataApi/common_lib/common/services/report_service.py
+++ services/api/dataApi/common_lib/common/services/report_service.py
@@ -1,19 +1,16 @@
 from common.repositories.reports import ReportsRepository
+from dataplattform.common.aws import SNS
 from common.services.athena_engine import process_sql
 from typing import Dict
 from functools import reduce
 import re
-import boto3
 import json
 from os import environ
 
 
 def pub_new_report(report: str):
-    sns = boto3.resource('sns')
-    topic = sns.Topic(environ.get('NEW_REPORT_TOPIC'))
-    topic.publish(
-        Message=json.dumps({'report': report}),
-        Subject='NewReport')
+    sns = SNS(environ.get('NEW_REPORT_TOPIC'))
+    sns.publish(json.dumps({'report': report}), 'NewReport')
 
 
 def new_report(new_report: Dict[str, str]):
