296d5f6d11175febe3231caa89773f1df85c43e2
Christopher Gjøvåg <christopher.gjovag@knowit.no>
2021-01-13 11:57:55 +0100

    Modify full_system as proof of concept

--- templates/full_system/process_lambda.py
+++ templates/full_system/process_lambda.py
@@ -1,12 +1,15 @@
-from dataplattform.common.handlers.process import ProcessHandler
+from dataplattform.common.handlers.process import PersonalProcessHandler
+from dataplattform.common.repositories.person_repository import PersonIdentifierType
 import pandas as pd
 from typing import Dict
 
 
-handler = ProcessHandler()
+handler = PersonalProcessHandler(PersonIdentifierType.ALIAS)
 
 
 @handler.process(partitions={})
 def process(data, events) -> Dict[str, pd.DataFrame]:
     out_df = pd.concat([pd.json_normalize(d.json()['data'])for d in data])
-    return {'some_structured_data': out_df}
+    return {
+        'olanor': {'some_structured_data': out_df}
+        }
