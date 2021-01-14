c0b3ba2ff5ba7a9afe249885a85bd7132f5b07d0
Christopher Gjøvåg <christopher.gjovag@knowit.no>
2021-01-14 15:44:28 +0100

    Change to use partitions instead

--- templates/full_system/process_lambda.py
+++ templates/full_system/process_lambda.py
@@ -1,15 +1,22 @@
-from dataplattform.common.handlers.process import PersonalProcessHandler
-from dataplattform.common.repositories.person_repository import PersonIdentifierType
+from dataplattform.common.handlers.process import PersonalProcessHandler, ProcessHandler
+from dataplattform.common.repositories.person_repository import PersonRepository, PersonIdentifierType
 import pandas as pd
 from typing import Dict
 
 
-handler = PersonalProcessHandler(PersonIdentifierType.ALIAS)
+# handler = PersonalProcessHandler(PersonIdentifierType.ALIAS)
+handler = ProcessHandler()
 
 
-@handler.process(partitions={})
+@handler.process(partitions={'person_data_test_4': ['guid']})
 def process(data, events) -> Dict[str, pd.DataFrame]:
-    out_df = pd.concat([pd.json_normalize(d.json()['data'])for d in data])
-    return {
-        'olanor': {'some_structured_data': out_df}
-        }
+
+    def transform_to_guid(data):
+        with PersonRepository() as repo:
+            for item in data:
+                item['guid'] = repo.get_guid_by(PersonIdentifierType.ALIAS, item['alias'])
+                del item['alias']
+        return data
+
+    out_df = pd.concat([pd.json_normalize(transform_to_guid(d.json()['data']))for d in data])
+    return {'person_data_test_4': out_df}
