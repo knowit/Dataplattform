bd8d1f037dd2e1b2fbee6313b2bb85295f966c39
HeleneSemmerud <helene.semmerud@knowit.no>
2021-01-15 13:40:08 +0100

    Adds unit-tests

--- services/ingestion/ubwCostumerPerResource/test_ubw_costumer_per_resource_process_lambda.py
+++ services/ingestion/ubwCostumerPerResource/test_ubw_costumer_per_resource_process_lambda.py
@@ -42,3 +42,66 @@ def test_process_data(create_table_mock, setup_queue_event, test_data):
         'ubw_costumer_per_resource',
         'reg_period',
         pd.Series(['202053', '202053', '202053']))
+
+
+def test_process_data_test_weigth(create_table_mock, setup_queue_event, test_data):
+    event = setup_queue_event(
+        schema.Data(
+            metadata=schema.Metadata(timestamp=0),
+            data=test_data['data']))
+
+    handler(event, None)
+
+    create_table_mock.assert_table_data_column(
+        'ubw_costumer_per_resource',
+        'weigth',
+        pd.Series([1, 2, 1]))
+
+    create_table_mock.assert_table_data_column(
+        'ubw_costumer_per_resource',
+        'costumer',
+        pd.Series(['costumer 2', 'costumer 1', 'costumer 3']))
+
+
+def test_process_data_test_used_hrs_zero(create_table_mock, setup_queue_event, test_data):
+    test_data['data'][0]['used_hrs'] = 0
+
+    event = setup_queue_event(
+        schema.Data(
+            metadata=schema.Metadata(timestamp=0),
+            data=test_data['data']))
+
+    handler(event, None)
+
+    create_table_mock.assert_table_data_column(
+        'ubw_costumer_per_resource',
+        'weigth',
+        pd.Series([1, 1]))
+
+    create_table_mock.assert_table_data_column(
+        'ubw_costumer_per_resource',
+        'costumer',
+        pd.Series(['costumer 2', 'costumer 3']))
+
+
+def test_process_data_test_dataframe_content(create_table_mock, setup_queue_event, test_data):
+    event = setup_queue_event(
+        schema.Data(
+            metadata=schema.Metadata(timestamp=0),
+            data=test_data['data']))
+
+    handler(event, None)
+
+    create_table_mock.assert_table_data(
+        'ubw_costumer_per_resource',
+        pd.DataFrame({
+            'reg_period': ['202053', '202053', '202053'],
+            'alias': ['X1', 'X1', 'X2'],
+            'name': ['X1_name', 'X1_name', 'X2_name'],
+            'project_type': ['External Projects', 'Local Projects', 'Local Projects'],
+            'work_order': ['work order no 2', 'work order no 1', 'work order no 3'],
+            'work_order_description': ['Some work order desc.', 'Some work order desc.', 'Some work order desc.'],
+            'costumer': ['costumer 2', 'costumer 1', 'costumer 3'],
+            'time': [0, 0, 0],
+            'weigth': [1, 2, 1]
+            }))
