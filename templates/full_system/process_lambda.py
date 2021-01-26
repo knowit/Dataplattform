e70f598e648f19442c53a24f9fbfc474f047ceaf
Christopher Gjøvåg <christopher.gjovag@knowit.no>
2021-01-26 12:02:24 +0100

    Fix full_system template

--- templates/full_system/process_lambda.py
+++ templates/full_system/process_lambda.py
@@ -7,7 +7,7 @@ from typing import Dict
 handler = PersonDataProcessHandler(PersonIdentifierType.ALIAS)
 
 
-@handler.process(partitions={})
+@handler.process(partitions={}, person_data_tables=['person_data_test_5'])
 def process(data, events) -> Dict[str, pd.DataFrame]:
     out_df = pd.concat([pd.json_normalize(d.json()['data'])for d in data])
     return {'person_data_test_5': out_df}
