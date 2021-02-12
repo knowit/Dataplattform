955447f7c37c764f26c708e82b901d876eccf813
alfredronning <alfredronning@gmail.com>
2021-02-12 13:20:37 +0100

    unpin dependencies

--- packages/common/setup.py
+++ packages/common/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='dataplattform_common',
     packages=find_namespace_packages(include=['dataplattform.*']),
-    install_requires=['dataclasses-json==0.4', 'boto3', 's3fs==0.4', 'fastparquet==0.4', 'pandas'],
+    install_requires=['dataclasses-json', 'boto3', 's3fs==0.4', 'fastparquet', 'pandas'],
     zip_safe=False)
