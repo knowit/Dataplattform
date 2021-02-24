d766b478bda43d2f99409476baaf969db4c23040
alfredronning <alfredronning@gmail.com>
2021-02-24 12:40:40 +0100

    pin requests to get rid of dependency warning

--- packages/cli/setup.py
+++ packages/cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='dataplattform_cli',
     packages=find_namespace_packages(include=['dataplattform.*']),
-    install_requires=['boto3', 'moto==1.3.14', 'requests',
+    install_requires=['boto3', 'moto==1.3.14', 'requests==2.23',
                       'python-dateutil', 'pandas', 'fastparquet'],
     dependency_links=['file:../common', 'file:../query'],
     entry_points={
