32972957a4773dd50f8db4b851bbf84280ade570
HeleneSemmerud <helene.semmerud@knowit.no>
2021-03-02 09:50:56 +0100

    Adds new historical table to ad-poller

--- services/ingestion/activeDirectoryProcess/ad_process_lambda.py
+++ services/ingestion/activeDirectoryProcess/ad_process_lambda.py
@@ -4,16 +4,22 @@ import pandas as pd
 from os import environ
 from dataplattform.common.handlers.process import ProcessHandler
 from typing import Dict
-
+from datetime import datetime
 handler = ProcessHandler()
 
 
-@handler.process(partitions={}, overwrite=True, overwrite_all_versions=True)
+@handler.process(partitions={}, overwrite=True, overwrite_all_versions=True,
+                 historical_tables=['ad_number_of_employees'])
 def process(data, events) -> Dict[str, pd.DataFrame]:
     db = boto3.resource('dynamodb')
     person_data_table = db.Table(environ.get('PERSON_DATA_TABLE'))
     persons = pd.DataFrame(person_data_table.scan()['Items'])
     persons_filtered = persons[['guid', 'displayName', 'email', 'manager']]
+    number_of_employees = len(persons_filtered)
+    ad_num_employees = pd.DataFrame({'time': int(datetime.now().timestamp()),
+                                     'number_of_employees': number_of_employees}, index=[0])
+
     return {
-        'active_directory': persons_filtered
+        'active_directory': persons_filtered,
+        'ad_number_of_employees': ad_num_employees
     }
