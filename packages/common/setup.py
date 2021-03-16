e09509eb547f4c04c5884f48fd0d2c975389be0f
Christopher Gjøvåg <christopher.gjovag@knowit.no>
2021-03-16 15:03:10 +0100

    Add xray tracing to services

--- packages/common/setup.py
+++ packages/common/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='dataplattform_common',
     packages=find_namespace_packages(include=['dataplattform.*']),
-    install_requires=['dataclasses-json', 'boto3', 's3fs==0.4', 'fastparquet', 'pandas'],
+    install_requires=['dataclasses-json', 'boto3', 's3fs==0.4', 'fastparquet', 'pandas', 'aws-xray-sdk'],
     zip_safe=False)
