0f390f8e6d8f4d77c10e52f54caa8e0230a4c4db
felix halvorsen <felix.halvorsen@knowit.no>
2021-05-04 11:06:33 +0200

    Removed unused code from inital copy

--- services/api/dataApi/apis/managerquery.py
+++ services/api/dataApi/apis/managerquery.py
@@ -34,8 +34,8 @@ QueryRequest = ns.model(
     produces=['application/json', 'text/csv']
 )
 class Email(Resource):
-    def email(self,email, output_format):
-        sql = "select * from active_directory where email is " + email
+    def email(self, email_address, output_format):
+        sql = "select * from active_directory where email is " + email_address
         df = engine.execute(sql)
 
         if output_format == 'csv':
@@ -46,73 +46,3 @@ class Email(Resource):
             return Response(
                 df.to_json(orient='records'),
                 content_type='application/json')
-
-
-@ns.route('/query', strict_slashes=False)
-@ns.doc(
-    responses={400: 'Validation Error'},
-    produces=['application/json', 'text/csv']
-)
-class Query(Resource):
-    def query(self, sql, output_format):
-        df = engine.execute(sql)
-
-        if output_format == 'csv':
-            return Response(
-                df.to_csv(index=False),
-                content_type='text/csv')
-        else:
-            return Response(
-                df.to_json(orient='records'),
-                content_type='application/json')
-
-    @ns.expect(parser)
-    @ns.doc(security={'oauth2': ['openid']})
-    def get(self):
-        args = parser.parse_args()
-        try:
-            return self.query(args.sql, args.output_format)
-        except Exception as e:
-            ns.abort(500, e)
-
-    @ns.expect(QueryRequest)
-    @ns.doc(security={'oauth2': ['openid']})
-    def post(self):
-        try:
-            return self.query(ns.payload['sql'], ns.payload.get('format', 'json'))
-        except Exception as e:
-            ns.abort(500, e)
-
-
-class FilterType:
-    __schema__ = {'type': 'string', 'format': 'field:value'}
-
-    def __call__(self, value: str):
-        field, _, value = value.partition(':')
-        return field, value
-
-
-filter_parser = ns.parser()
-filter_parser.add_argument(
-    'filter',
-    type=FilterType(),
-    dest='filter',
-    action='append')
-
-
-@ns.route('/query/report/<name>', strict_slashes=False)
-class QueryReport(Resource):
-    @ns.expect(filter_parser)
-    @ns.doc(security={'oauth2': ['openid']})
-    def get(self, name):
-        filters = filter_parser.parse_args().get('filter', [])
-
-        with ReportsRepository() as repo:
-            report = repo.get(name)
-
-        query = cache_table_service.query_cache_table(
-            report['dataProtection'], report['name'])
-
-        return Response(
-            query(filters),
-            content_type='application/json')
