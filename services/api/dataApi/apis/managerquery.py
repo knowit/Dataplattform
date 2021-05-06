e7b97796c7413fa9a7df451edc1d23c152ca0346
felix halvorsen <felix.halvorsen@knowit.no>
2021-05-06 11:28:36 +0200

    Removed uneccesary arguments

--- services/api/dataApi/apis/managerquery.py
+++ services/api/dataApi/apis/managerquery.py
@@ -13,31 +13,23 @@ parser.add_argument(
     type=str,
     dest='email',
     required=True)
-parser.add_argument(
-    'format',
-    type=str,
-    dest='output_format',
-    default='json',
-    choices=['json', 'csv'])
 
 EmployeeModel = ns.model(
     'Employee', {
         'email': fields.String(),
-        'format': fields.String(default='json', enum=['json', 'csv']),
     })
 
 
-@ns.route('/<email>', strict_slashes=False)
+@ns.route('/email', strict_slashes=False)
 @ns.doc(
     responses={400: 'Validation Error'},
     produces=['application/json', 'text/csv']
 )
 class Email(Resource):
-    def email(self, email_address, output_format,email):
-        'select a.guid,a.displayname,a.email,b.guid,b.displayname,b.email,c.guid,c.displayname,c.email ' +
-        'from active_directory a left outer join active_directory b on a.managerguid = b.guid left outer join active_directory c on b.managerguid = c.guid where a.email=\''+email+'\''
-        sql = "select * from active_directory where email is " + email_address
-        #df = engine.execute(sql)
+    def email(self, email_address):
+        emp_sql = 'select a.guid,a.displayname,a.email,b.guid,b.displayname,b.email,c.guid,c.displayname,c.email ' + 'from active_directory a left outer join active_directory b on a.managerguid = b.guid left outer join active_directory c on b.managerguid = c.guid where a.email=\''+email_address+'\''
+        #sql = "select * from active_directory where email is " + email_address
+        df = engine.execute(emp_sql)
         data_json = df.to_json(orient='records')
 
         def setUser(data, keys , n):
@@ -58,21 +50,16 @@ class Email(Resource):
             user_details = setUser(user,keys,0)
             list_of_users.append(user_details)
 
-        if output_format == 'csv':
-            return Response(
-                df.to_csv(index=False),
-                content_type='text/csv')
-        else:
-            return Response(
-                list_of_users,
-                content_type='application/json')
+        return Response(
+            list_of_users,
+            content_type='application/json')
 
     @ns.expect(parser)
     @ns.doc(security={'oauth2': ['openid']})
     @ns.doc(model=EmployeeModel)
-    def get(self,email):
+    def get(self):
         args = parser.parse_args()
         try:
-            return self.email(args.email, args.output_format,email)
+            return self.email(args.email)
         except Exception as e:
             ns.abort(500, e)
