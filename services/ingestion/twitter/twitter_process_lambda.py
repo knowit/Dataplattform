a3cbf28a13c8a03ab5085bbfe1be8e3f59233898
Levi Sørum <levi.sorum@gmail.com>
2021-06-02 14:04:55 +0200

    Lint on twitter.

--- services/ingestion/twitter/twitter_process_lambda.py
+++ services/ingestion/twitter/twitter_process_lambda.py
@@ -23,20 +23,28 @@ def process(data, events) -> Dict[str, pd.DataFrame]:
     search_data, timeline_data, accounts_data = list(zip(*[
         (
             d['data']['search'], d['data']['timeline'],
-            [dict(x, time=int(d['metadata']['timestamp'])) for x in d['data']['accounts']]
+            [dict(x, time=int(d['metadata']['timestamp']))
+             for x in d['data']['accounts']]
         )
         for d in [d.json() for d in data]]))
 
-    search_data = pd.concat([pd.DataFrame.from_records(d) for d in search_data])
-    timeline_data = pd.concat([pd.DataFrame.from_records(d) for d in timeline_data])
-    accounts_data = pd.concat([pd.DataFrame.from_records(d) for d in accounts_data])
+    search_data = pd.concat([pd.DataFrame.from_records(d)
+                            for d in search_data])
+    timeline_data = pd.concat([pd.DataFrame.from_records(d)
+                              for d in timeline_data])
+    accounts_data = pd.concat([pd.DataFrame.from_records(d)
+                              for d in accounts_data])
 
     try:
-        in_tweets_df = ath.from_('twitter_tweets').select('tweet_id').execute(ath).as_pandas()
-        in_timeline_df = ath.from_('twitter_timeline').select('tweet_id').execute(ath).as_pandas()
-        search_data = search_data[~search_data.tweet_id.isin(in_tweets_df.tweet_id)]
-        timeline_data = timeline_data[~timeline_data.tweet_id.isin(in_timeline_df.tweet_id)]
-        
+        in_tweets_df = ath.from_('twitter_tweets').select(
+            'tweet_id').execute(ath).as_pandas()
+        in_timeline_df = ath.from_('twitter_timeline').select(
+            'tweet_id').execute(ath).as_pandas()
+        search_data = search_data[~search_data.tweet_id.isin(
+            in_tweets_df.tweet_id)]
+        timeline_data = timeline_data[~timeline_data.tweet_id.isin(
+            in_timeline_df.tweet_id)]
+
     except OperationalError as err:  # Workaround for inital construction of tables when changing names
         print(f'OperationalError {err}')
 
