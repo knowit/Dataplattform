cc9caa7fc058e8a64eb133b4bfb952aab57cbd08
Frikk Felix Egge Fjuk <frikk.felix@gmail.com>
2021-06-09 13:27:35 +0200

    Remove boilerplate code from test, add tests for table content

--- services/ingestion/kompetanseKartlegging/tests/test_process_lambda.py
+++ services/ingestion/kompetanseKartlegging/tests/test_process_lambda.py
@@ -31,13 +31,18 @@ def setup_queue_event(s3_bucket):
     yield make_queue_event
 
 
-def test_initial_process(setup_queue_event, test_data, create_table_mock, dynamodb_resource):
+@fixture(autouse=True)
+def invoke_process_handler(create_table_mock, setup_queue_event, test_data, dynamodb_resource):
     event = setup_queue_event(
         schema.Data(
             metadata=schema.Metadata(timestamp=0),
-            data=test_data['data']))
+            data=test_data['data']
+        )
+    )
+    return handler(event, None)
 
-    handler(event, None)
+
+def test_initial_process(create_table_mock):
     create_table_mock.assert_table_created(
         'kompetansekartlegging_users',
         'kompetansekartlegging_answers',
@@ -47,41 +52,63 @@ def test_initial_process(setup_queue_event, test_data, create_table_mock, dynamo
     )
 
 
-def test_process_users_content(setup_queue_event, test_data, create_table_mock, dynamodb_resource):
-    event = setup_queue_event(
-        schema.Data(
-            metadata=schema.Metadata(timestamp=0),
-            data=test_data['data']))
-
-    handler(event, None)
+def test_process_users_content(create_table_mock):
     create_table_mock.assert_table_data_contains_df(
         'kompetansekartlegging_users',
         pd.DataFrame({
             'username': ['2c54bb77-190a-4651-9009-7c9fab39a03a', '3c2af0ad-19f0-4f0d-b6dc-031dfcaa423b'],
             'email': ['per.nordmann@knowit.no', 'kari.nordmann@knowit.no'],
             'guid': ['20dbbfa18380233aa643575720b893fac5137699', '491b9fa9bfac17563882b0fdc6f3a8a97417bd99']
-        }))
+        })
+    )
 
 
-def test_process_answers_content(setup_queue_event, test_data, create_table_mock, dynamodb_resource):
-    event = setup_queue_event(
-        schema.Data(
-            metadata=schema.Metadata(timestamp=0),
-            data=test_data['data']))
-
-    handler(event, None)
+def test_process_answers_content(create_table_mock):
     create_table_mock.assert_table_data_contains_df(
         'kompetansekartlegging_answers',
         pd.DataFrame({
             'email': ['per.nordmann@knowit.no'],
             'questionId': ['d1862cf3-7123-4d35-9cfd-6587e97dc0c8'],
-            'unanswered': [True],
             'knowledge': [None],
             'motivation': [None],
             'updatedAt': [None],
             'customScaleValue': [None],
-            'scaleStart': [None],
-            'scaleMiddle': [None],
-            'scaleEnd': [None],
             'guid': ['20dbbfa18380233aa643575720b893fac5137699']
-        }))
+        })
+    )
+
+
+def test_process_catalogs_content(create_table_mock):
+    create_table_mock.assert_table_data_contains_df(
+        'kompetansekartlegging_catalogs',
+        pd.DataFrame({
+            'id': ['fb9e8f06-4cfa-463e-9e8f-cc0853464070', '63371236-3116-4e6d-85aa-168f6d34d18b', None],
+            'label': ['2021V', None, 'no_id'],
+        })
+    )
+
+
+def test_process_categories_content(create_table_mock):
+    create_table_mock.assert_table_data_contains_df(
+        'kompetansekartlegging_categories',
+        pd.DataFrame({
+            'index': [5, 8],
+            'text': ['AI og Data Engineering', 'Smidig metodikk og produktledelse'],
+            'description': ['ML og datadrevne loesninger', 'Smidig metodikk og produktledelse'],
+            'id': ['84baa82a-bbad-4fed-9bd1-85c9f50fdcfc', 'e99c1908-3da3-4e29-95e9-cfd5bfc41a57']
+        })
+    )
+
+
+def test_process_questions_content(create_table_mock):
+    create_table_mock.assert_table_data_contains_df(
+        'kompetansekartlegging_questions',
+        pd.DataFrame({
+            'index': [92, 76],
+            'text': ['Microsoft Azure', 'Rammeverk som tensorflow, keras, pytorch, mm'],
+            'id': ['9f40d807-3fa6-4446-bdc2-7681ca136b3b', 'be02bf31-e082-41c9-a9eb-eb4c60c77f8c'],
+            'topic': ['Azure', 'ML-rammeverk'],
+            'categoryID': ['3efffcf3-b458-467d-bdac-e4ae08b3f040', '84baa82a-bbad-4fed-9bd1-85c9f50fdcfc'],
+            'type': ['knowledgeMotivation', 'knowledgeMotivation']
+        })
+    )
