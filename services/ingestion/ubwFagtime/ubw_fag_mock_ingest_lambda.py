49482849e7bbea2aae811e25fcf3b68ccd43065e
kristjgr <kristinejun@hotmail.com>
2021-06-15 11:31:49 +0200

    added empty mock for ubw fagtime

---
+++ services/ingestion/ubwFagtime/ubw_fag_mock_ingest_lambda.py
@@ -0,0 +1,17 @@
+from dataplattform.common.handlers.ingest import IngestHandler
+import json
+from dataplattform.common.schema import Data, Metadata
+from datetime import datetime
+
+handler = IngestHandler()
+
+
+@handler.ingest()
+def mock(event, context) -> Data:
+    test_data = open('tests/test_data.json')
+    test_json = json.load(test_data)
+
+    return Data(
+        metadata=Metadata(timestamp=datetime.now().timestamp()),
+        data=test_json['data']
+    )
