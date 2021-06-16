39e67c79e4b643ec471fc0e0a16b333306ad007a
Frikk Felix Egge Fjuk <frikk.felix@gmail.com>
2021-06-16 14:23:34 +0200

    Implement dynamic changing of api url

--- services/ingestion/kompetanseKartlegging/kompetansekartlegging_ingest_lambda.py
+++ services/ingestion/kompetanseKartlegging/kompetansekartlegging_ingest_lambda.py
@@ -3,23 +3,31 @@ from dataplattform.common.schema import Data, Metadata
 from dataplattform.common.aws import SSM
 from datetime import datetime
 import requests
-
-base_url = 'https://api.kompetanse.knowit.no/dev'
-# base_url = 'https://api.kompetanse.knowit.no'
+from os import environ
 
 handler = IngestHandler()
 
 
 @handler.ingest()
 def ingest(event) -> Data:
-    api_token = SSM(with_decryption=True).get('kompetansekartlegging_api_key_dev')
+    base_url = 'https://api.kompetanse.knowit.no'
+    stage = environ['STAGE']
+
+    if stage == 'dev':
+        base_url += '/dev'
+
+    api_token = SSM(with_decryption=True).get(f'kompetansekartlegging_api_key_{stage}')
     users = requests.get(f'{base_url}/users', headers={'x-api-key': api_token}).json()
     answers = requests.get(f'{base_url}/answers', headers={'x-api-key': api_token}).json()
     catalogs = requests.get(f'{base_url}/catalogs', headers={'x-api-key': api_token}).json()
 
     newest_catalog = answers[0]['formDefinitionID']
-    categories = requests.get(f'{base_url}/catalogs/{newest_catalog}/categories', headers={'x-api-key': api_token}).json()
-    questions = requests.get(f'{base_url}/catalogs/{newest_catalog}/questions', headers={'x-api-key': api_token}).json()
+
+    categories = requests.get(f'{base_url}/catalogs/{newest_catalog}/categories',
+                              headers={'x-api-key': api_token}).json()
+
+    questions = requests.get(f'{base_url}/catalogs/{newest_catalog}/questions',
+                             headers={'x-api-key': api_token}).json()
 
     return Data(
         metadata=Metadata(timestamp=datetime.now().timestamp()),
