f385efa17194f99f89babeb1a398f12838405ed6
Levi Sørum <levi.sorum@gmail.com>
2021-06-18 10:01:21 +0200

    Changed auth url depending on stage.

--- services/api/dataApi/app.py
+++ services/api/dataApi/app.py
@@ -10,8 +10,8 @@ from apis.report import ns as report_ns
 from apis.catalogue import ns as catalogue_ns
 
 
-auth_url = 'dev-auth.new-dev.dataplattform.knowit.no' \
-    if environ.get('STAGE', 'dev') == 'dev' else 'auth.new-dev.dataplattform.knowit.no'
+auth_url = 'auth.new-dev.dataplattform.knowit.no' \
+    if environ.get('STAGE', 'dev') == 'dev' else 'auth.dataplattform.knowit.no'
 
 app = Flask(__name__)
 
