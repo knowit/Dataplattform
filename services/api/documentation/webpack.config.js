cd8dbdf7e4dd72ba0f036ad2b5267e0687f4f906
Levi Sørum <levi.sorum@gmail.com>
2021-06-29 14:20:49 +0200

    Fixed dynamic clientId and apiUrl.

--- services/api/documentation/webpack.config.js
+++ services/api/documentation/webpack.config.js
@@ -3,29 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { DefinePlugin } = require('webpack')
-const { SSMClient, GetParameterCommand} = require("@aws-sdk/client-ssm");
+const fs = require("fs")
 
 const outputPath = path.resolve(__dirname, 'dist');
 
-// Set the AWS Region.
-const REGION = "REGION"; //e.g. "us-east-1"
-// Create an Amazon S3 service client object.
 
-const ssmClient = new SSMClient({ region: 'eu-central-1' });
+const serverlessState = JSON.parse(fs.readFileSync(".serverless/serverless-state.json", "utf-8"))
 
-async function getClientId() {
-  const input =  {"Name" : '/dev/cognito/UserPoolClientId'}
-//const input2 = {name: '/dev/cloudfront-api-dist/distributionAlias'}
-  const command = new GetParameterCommand(input)
-//const command2 = new GetParameterCommand(input2)
+const { cognitoClientId, apiUrl } = serverlessState.service.custom
 
-  var result = 0;
-  const clientid = await ssmClient.send(command);
-  //const apiurl = await ssmClient.send(command2);
-  return clientid;
-}
-
-const clientid = getClientId()
 
 module.exports = {
   mode: 'development',
@@ -66,11 +52,8 @@ module.exports = {
       template: 'index.html'
     }),
     new DefinePlugin({
-      API_URL: JSON.stringify('api.new-dev.dataplattform.knowit.no'), //TODO: Ref
-      //DEMO_CLIENT_ID: JSON.stringify('5dk4t7d9ad5l3hcmo2fq14is81') // TODO: Ref
-      DEMO_CLIENT_ID: JSON.stringify((() => {console.log(clientid.Parameter.Value);
-                                                          return clientid.Parameter.Value})()) // TODO: Ref
-      //DEMO_CLIENT_ID: JSON.stringify('27g4krd7m209soqhk050hoa0bn')
+      API_URL: JSON.stringify(apiUrl),
+      DEMO_CLIENT_ID: JSON.stringify(cognitoClientId)
     })
   ],
   output: {
