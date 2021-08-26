5faba5febf935fa6002fe1e9de6c38133a2110f2
Bjørn Magnus Valberg Iversen <bjorn.iversen@knowit.no>
2021-08-26 15:27:28 +0200

    initialize env vars before importing dataplattform.common.aws

--- services/ingestion/gitHub/generate_test_event.py
+++ services/ingestion/gitHub/generate_test_event.py
@@ -2,12 +2,12 @@ import json
 import hmac
 from hashlib import sha1
 from json import dumps
-from dataplattform.common.aws import SSM
 import os
-
-
-os.environ['STAGE'] = 'dev'
-os.environ['SERVICE'] = 'github'
+# must be done before dataplattform.common.aws is imported
+os.environ["AWS_XRAY_SDK_ENABLED"] = "false"
+os.environ["STAGE"] = "dev"
+os.environ["SERVICE"] = "github"
+from dataplattform.common.aws import SSM
 
 with open("tests/test_data/test_webhook_ingest_data.json") as f:
     body = json.load(f)
