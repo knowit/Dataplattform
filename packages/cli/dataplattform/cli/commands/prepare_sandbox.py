926e1cfca612b0d32b7845640539adddf85ba857
Bjørn Magnus Valberg Iversen <bjorn.iversen@knowit.no>
2021-10-07 17:28:08 +0200

    finalize cli script

--- packages/cli/dataplattform/cli/commands/prepare_sandbox.py
+++ packages/cli/dataplattform/cli/commands/prepare_sandbox.py
@@ -6,24 +6,14 @@ from typing import List
 
 CERTIFICATE_REGION = 'us-east-1'
 
-def init(parser: ArgumentParser):
-    parser.add_argument("--domain-name", required=True)
-    parser.add_argument("--stage", default='dev', choices=['dev', 'prod'])
-
-def create_domain_parameter(domain_name: str, stage: str):
+def create_parameter(name: str, value: str):
     client = boto3.client('ssm')
     client.put_parameter(
-        Name=f"/{stage}/routes/domain_name",
-        Value=domain_name,
+        Name=name,
+        Value=value,
         Type='String',
         Overwrite=True
     )
-
-def write_name_servers_to_file(path: str, name_servers: List[str]) -> None:
-    with open(path, 'w') as f:
-        for ns in name_servers:
-            f.write(ns + '\n')
-
 
 def create_certificate(domain_name: str) -> dict:
     client = boto3.client('acm', region_name=CERTIFICATE_REGION)
@@ -34,20 +24,14 @@ def create_certificate(domain_name: str) -> dict:
     )
     return response
 
-def create_certificate_parameter(cert_id: str, stage: str):
-    client = boto3.client('ssm')
-    client.put_parameter(
-        Name=f"/{stage}/USEA1/sslIdentifier",
-        Value=cert_id,
-        Type='String',
-        Overwrite=True
-    )
-
 def create_hosted_zone(domain_name: str) -> dict:
     client = boto3.client('route53')
     response = client.create_hosted_zone(
         Name=domain_name,
-        CallerReference=str(datetime.datetime.now())
+        CallerReference=str(datetime.datetime.now()),
+        HostedZoneConfig={
+            'Comment': 'For validering av SSL-sertifikatet før tjenestene deployes til sandbox.'
+        },
     )
     return response
 
@@ -79,13 +63,19 @@ def create_record_for_validation(hosted_zone_id: str, cert_arn: str) -> None:
         }
     )
 
+def write_name_servers_to_file(path: str, name_servers: List[str]) -> None:
+    with open(path, 'w') as f:
+        for ns in name_servers:
+            f.write(ns + '\n')
 
-
+def init(parser: ArgumentParser):
+    parser.add_argument("--domain-name", required=True)
+    parser.add_argument("--stage", default='dev', choices=['dev', 'prod'])
 
 def run(args: Namespace, _):
     cert_response = create_certificate(args.domain_name)
     hosted_zone_response = create_hosted_zone(args.domain_name)
     create_record_for_validation(hosted_zone_response['HostedZone']['Id'], cert_response['CertificateArn'])
-    create_domain_parameter(args.domain_name, args.stage)
-    create_certificate_parameter(cert_response['CertificateArn'].split(':')[-1], args.stage)
+    create_parameter(f"/{args.stage}/USEA1/sslIdentifier", cert_response['CertificateArn'].split(':')[-1])
+    create_parameter(f"/{args.stage}/routes/domain_name", args.domain_name)
     write_name_servers_to_file('my_name_servers.txt', hosted_zone_response['DelegationSet']['NameServers'])
