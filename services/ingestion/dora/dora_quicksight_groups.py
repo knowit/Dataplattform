e3761682d82f7ce8b3c4cf927a53ab191cbf7532
EvenKronenJohansen <even.johansen@knowit.no>
2021-10-26 09:00:00 +0200

    Added functionality to automatically add new repos to a csv role file

--- services/ingestion/dora/dora_quicksight_groups.py
+++ services/ingestion/dora/dora_quicksight_groups.py
@@ -1,13 +1,17 @@
 import boto3
+import botocore
 import csv
+import pandas as pd
+import io
 
 quicksight_client = boto3.client('quicksight')
 glue_client = boto3.client('glue')
 sts_client = boto3.client('sts')
+s3_client = boto3.client('s3')
 s3 = boto3.resource('s3')
-bucket = s3.Bucket('BUCKET_NAME')
+bucket = s3.Bucket('dev-datalake-bucket-876363704293')
 
-key = 'KEY_PATH'
+key = 'quicksight-rolebindings.csv'
 
 def handler(event, context):
 
@@ -28,7 +32,6 @@ def handler(event, context):
             list_of_groups.append(group['GroupName'])
         return list_of_groups
 
-
     def get_group_names(response, existing_groups):
         list_of_groups = []
         for group in response:
@@ -42,7 +45,6 @@ def handler(event, context):
             list_of_existing_groups.append(partition)
         return list_of_existing_groups
 
-
     def create_groups(partitions):
         for partition in partitions:
             quicksight_client.create_group(
@@ -52,28 +54,49 @@ def handler(event, context):
                 Namespace = 'default'
             )
 
-    def update_rolebinding(rolebindings):
+    def download_csv_to_temp(filename, bucketname):
         local_file_name = '/tmp/rolebinding.csv'
-        s3.Bucket('BUCKET_NAME').download_file(key, local_file_name)
+        s3.Bucket(bucketname).download_file(key, local_file_name)
 
-        new_rolebindings = rolebindings
+    def read_rolebindings(filename, bucketname):
+        csv_obj = s3_client.get_object(Bucket=bucketname, Key=filename)
+        csv_string = csv_obj['Body'].read().decode('utf-8')
+        df = pd.read_csv(io.StringIO(csv_string))
+        list_of_roles = []
+        for roles in df.Role:
+            list_of_roles.append(roles)
+        return list_of_roles
 
-        with open('tmp/rolebinding.csv', 'r') as infile:
-            reader = list(csv.reader(infile))
-            reader.insert(0,new_rolebindings)
+    def write_to_csv(dataframes, filename):
+        df = pd.concat(dataframes)
+        df.to_csv('/tmp/' + filename + '.csv', mode = 'a', index = False, header = True)
 
-        with open('tmp/rolebindings.csv', 'w', newline='') as outfile:
-            writer = csv.writer(outfile)
-            for line in reader:
-                writer.writerow(line)
+    def add_to_dataframe(group):
+        df = pd.DataFrame({
+            'Group': [group],
+            'partition_1': [group]
+        })
+        return df
 
-        bucket.upload_file('/tmp/rolebindings.csv', key)
+    def update_rolebinding(filename, bucketname):
+        existing_group = get_existing_groups(response_groups)
+        download_csv_to_temp(filename, bucketname)
+        existing_rolebindings = read_rolebindings(filename, bucketname)
+
+        list_of_df = []
+        for group in existing_group:
+            if group not in existing_rolebindings:
+                list_of_df.append(add_to_dataframe(group))
+        
+        write_to_csv(list_of_df, filename)
+
+        bucket.upload_file('/tmp/' + filename + '.csv', key)
 
     glue_groups = get_new_groups()
     existing_group = get_existing_groups(response_groups)
     new_groups = get_group_names(glue_groups, existing_group)
     create_groups(new_groups)
-    #update_rolebinding(new_groups)
+    update_rolebinding("quicksight-rolebindings.csv", 'dev-datalake-bucket-876363704293')
 
     return {
         'message': 'success!'
