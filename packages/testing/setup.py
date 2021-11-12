91cf85f448c9b6f0d76c216993714a8a0986bcee
bjorn-iversen <bjorn.iversen@knowit.no>
2021-11-12 14:00:41 +0100

    use same boto3 and botocore versions as aws

--- packages/testing/setup.py
+++ packages/testing/setup.py
@@ -5,5 +5,5 @@ setup(
     packages=find_namespace_packages(include=['dataplattform.*']),
     entry_points={"pytest11": ["dataplattform = dataplattform.testing.plugin"]},
     install_requires=['pytest==6.2.5', 'pytest-env==0.6.2', 'pytest-mock==3.6.1', 'moto==1.3.14',
-                      'dataclasses-json==0.5.6', 'boto3==1.20.3', 'botocore==1.23.3', 'pyathena==1.11'],
+                      'dataclasses-json==0.5.6', 'boto3==1.18.55', 'botocore==1.21.55', 'pyathena==1.11'],
     zip_safe=False)
