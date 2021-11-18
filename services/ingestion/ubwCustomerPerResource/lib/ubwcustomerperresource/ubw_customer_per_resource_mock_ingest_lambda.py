e9c374dae44faa64dd255bbc7fc5a66659ef9f5c
Bjørn Magnus Valberg Iversen <bjorn.iversen@knowit.no>
2021-11-18 09:32:14 +0100

    fix: correct test data path

--- services/ingestion/ubwCustomerPerResource/lib/ubwcustomerperresource/ubw_customer_per_resource_mock_ingest_lambda.py
+++ services/ingestion/ubwCustomerPerResource/lib/ubwcustomerperresource/ubw_customer_per_resource_mock_ingest_lambda.py
@@ -2,13 +2,15 @@ from dataplattform.common.handlers.ingest import IngestHandler
 import json
 from dataplattform.common.schema import Data, Metadata
 from datetime import datetime
+from pathlib import Path
 
 handler = IngestHandler()
 
 
 @handler.ingest()
 def mock(event) -> Data:
-    with open('../../tests/test_data.json') as f:
+    test_data_path = Path(__file__).resolve().parent.parent.parent / Path('tests') / Path('test_data.json')
+    with open(test_data_path) as f:
         test_json = json.load(f)
 
     return Data(
