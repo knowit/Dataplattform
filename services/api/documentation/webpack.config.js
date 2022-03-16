f70c72a64afa8fcd4ca615a3f52c72a9b7500901
thomasduy <thomas_duy@hotmail.com>
2022-03-16 14:22:05 +0100

    added spacing

--- services/api/documentation/webpack.config.js
+++ services/api/documentation/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { DefinePlugin, ProvidePlugin } = require('webpack')
 const fs = require("fs")
