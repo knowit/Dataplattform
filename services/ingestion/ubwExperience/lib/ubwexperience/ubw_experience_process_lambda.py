95c488380e00a07840714e0035a8430013c55a9c
Ole Boe <ole.boe@knowit.no>
2022-09-22 09:09:35 +0200

    Change experience calculation

--- services/ingestion/ubwExperience/lib/ubwexperience/ubw_experience_process_lambda.py
+++ services/ingestion/ubwExperience/lib/ubwexperience/ubw_experience_process_lambda.py
@@ -3,6 +3,7 @@ from dataplattform.common.aws import S3
 from dataplattform.common.handlers.process import ProcessHandler
 from typing import Dict
 from datetime import datetime
+from datetime import date
 import os
 import pandas as pd
 import numpy as np
@@ -30,6 +31,7 @@ def process(data, events) -> Dict[str, pd.DataFrame]:
     col_selection_table = [
         "resource_id",
         "name",
+        "experience",
         "r0_x0023_exami0_x0023_612",
         "xr0_x0023_e1_x0023_611_x0023_U00",
         "date_from"
@@ -38,21 +40,25 @@ def process(data, events) -> Dict[str, pd.DataFrame]:
     experience_df.rename(columns={
         'r0_x0023_exami0_x0023_612': 'examination_year',
         'xr0_x0023_e1_x0023_611_x0023_U00': 'grade',
-        'resource_id': 'alias'}, inplace=True)
+        'resource_id': 'alias',
+        'date_from': 'start_year'}, inplace=True)
 
     def get_experience(data):
         experiences = []
+        current_year = date.today().year
         for (index, row) in data.iterrows():
-            examination_year = row["examination_year"]
-            work_start = int(row["date_from"][:4])
-            if pd.isna(examination_year) or work_start == 0:
-                experience = -1
-            else:
-                experience = int(work_start) - int(examination_year)
-
             person = dict(row)
+
+            examination_year = row["examination_year"]
+            
+            if not pd.isna(examination_year) and examination_year != "0":
+                experience = current_year - int(examination_year)
+            else:
+                experience = 0
+
             person["experience"] = experience
             experiences.append(person)
+
         return pd.DataFrame(experiences)
 
     ubw_experience = get_experience(experience_df)
