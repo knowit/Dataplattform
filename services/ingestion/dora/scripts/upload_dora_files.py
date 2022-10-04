1aa07b2c3248857cb6721484f437af0ce272383c
Levi Sørum <levi.sorum@gmail.com>
2022-10-04 15:42:50 +0200

    Skriv om dora-tjenester slik at de kan deployes mot prod. (#423)
    * Skriv om dora-tjenester slik at de kan deployes mot prod.
    
    * Fjern unødvendig Sub tagg fra athena

--- services/ingestion/dora/scripts/upload_dora_files.py
+++ services/ingestion/dora/scripts/upload_dora_files.py
@@ -1,44 +1,45 @@
 import boto3
 from botocore.exceptions import ClientError
-import logging
 import json
 
-s3 = boto3.resource('s3')
-s3_client = boto3.client('s3')
-sts_client = boto3.client('sts')
+session = boto3.Session()
+s3 = session.resource('s3')
+s3_client = session.client('s3')
+sts_client = session.client('sts')
 account_response = str(sts_client.get_caller_identity()['Account'])
-bucket = f"datalake-bucket-{account_response}"
+datalake_bucket = f"datalake-bucket-{account_response}"
+stage = "dev"
 location = "data/level-3/dora/"
 path = "../misc-files/"
 
 
 def get_stage(bucket):
-    session = boto3.session.Session()
     s3_resource = session.resource('s3')
-    if s3_resource.meta.client.head_bucket(Bucket=f"dev-{bucket}"):
-        return "dev-"
-    elif s3_resource.meta.client.head_bucket(Bucket=f"prod-{bucket}"):
-        return "prod-"
-    else:
-        return False
+    try:
+        s3_resource.meta.client.head_bucket(Bucket=f"dev-{bucket}")
+        return "dev"
+    except ClientError:
+        try:
+            s3_resource.meta.client.head_bucket(Bucket=f"prod-{bucket}")
+            return "prod"
+        except ClientError as e:
+            raise Exception(e)
 
 
 def upload_file(file_name, bucket):
-    stage = get_stage(bucket)
     try:
-        response = s3_client.upload_file(path + file_name, f"{stage}{bucket}", location + file_name)
+        response = s3_client.upload_file(path + file_name, f"{bucket}", location + file_name)
+        return response
     except ClientError as e:
-        logging.error(e)
-    return response
+        raise Exception(e)
 
 
 def upload_frequency(file_name, bucket):
-    stage = get_stage(bucket)
     try:
-        response = s3_client.upload_file(path + file_name, f"{stage}{bucket}", location + 'frequency/' + file_name)
+        response = s3_client.upload_file(path + file_name, f"{bucket}", location + 'frequency/' + file_name)
+        return response
     except ClientError as e:
-        logging.error(e)
-    return response
+        raise Exception(e)
 
 
 def get_bucket(bucket):
@@ -57,7 +58,8 @@ def create_manifest():
         "fileLocations": [
             {
                 "URIs": [
-                    "s3://dev-datalake-bucket-" + account_response + "/data/level-3/dora/quicksight_role_bindings.csv"
+                    "s3://" + stage + "-datalake-bucket-" + account_response + "/data/level-3/dora"
+                                                                               "/quicksight_role_bindings.csv "
                 ]
             }
         ],
@@ -70,16 +72,18 @@ def create_manifest():
     }
 
     json_string = json.dumps(manifest_data, indent=2)
-    jsonFile = open("../misc-files/manifest.json", "w")
-    jsonFile.write(json_string)
-    jsonFile.close()
+    json_file = open("../misc-files/manifest.json", "w+")
+    json_file.write(json_string)
+    json_file.close()
 
 
 if __name__ == "__main__":
-    stage = get_stage(bucket)
+    stage = get_stage(datalake_bucket)
     create_manifest()
-    print(isfile_s3(f"{stage}{bucket}", "dora_users.csv"))
-    if not isfile_s3(f"{stage}{bucket}", "dora_usersa.csv"):
-        upload_file("dora_users.csv", f"{stage}{bucket}")
-    upload_file("manifest.json", f"{stage}{bucket}")
-    upload_frequency('frequency.csv', f"{stage}{bucket}")
+    if not isfile_s3(f"{stage}-{datalake_bucket}", "dora_users.csv"):
+        upload_file("dora_users.csv", f"{stage}-{datalake_bucket}")
+        print("Success")
+    else:
+        print(f"dora_users.csv already exists in bucket {stage}-{datalake_bucket}")
+    upload_file("manifest.json", f"{stage}-{datalake_bucket}")
+    upload_frequency('frequency.csv', f"{stage}-{datalake_bucket}")
